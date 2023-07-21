93b623b5689d39d4e3f2b50947f1e053986563b0
keroropper <somato.0311@gmail.com>
2023-07-22 01:12:46 +0900

    応募者一覧、応募一覧ページ完成

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -8,6 +8,9 @@ class Applicant < ApplicationRecord
   private
 
   def not_applicant_own_recruit
-    errors.add(:base, "You can't apply to your own recruit.") if user == recruit.user
+    return if recruit_id.nil?
+
+    recruit = Recruit.find(recruit_id)
+    errors.add(:base, "You can't apply to your own recruit.") if recruit.user_id == user_id
   end
 end
---
+++ spec/system/applicants_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Applicants", type: :system do
+  before do
+    create_applicants
+  end
+  let!(:user) { User.first }
+  let!(:recruit) { Recruit.first }
+
+  it '応募者一覧が表示でき、その応募者の詳細ページには承認ボタンがあること' do
+    sign_in(user)
+    visit recruit_applicants_index_path(recruit)
+    User.all[1..].each do |user|
+      expect(page).to have_content(user.name)
+    end
+    visit user_path(User.all[1])
+    expect(page).to have_content('承認する')
+
+    # 応募者でないユーザーページには承認ボタンがないこと
+    not_applicant_user = FactoryBot.create(:user)
+    visit user_path(not_applicant_user)
+    expect(page).to_not have_content('承認する')
+
+    # 自身のユーザーページには承認ボタンがないこと
+    visit user_path(user)
+    expect(page).to_not have_content('承認する')
+  end
+end
