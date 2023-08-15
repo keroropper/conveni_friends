09314fc006564d2f705f870b1b5fdd5f4c4c6bfe
keroropper <somato.0311@gmail.com>
2023-08-15 18:19:25 +0900

    微修正

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e 
 
 rm -f /app/tmp/pids/server.pid
-RAILS_ENV=production bin/rails db:create
-RAILS_ENV=production bin/rails db:migrate
-RAILS_ENV=production bin/rails db:seed
+# RAILS_ENV=production bin/rails db:create
+# RAILS_ENV=production bin/rails db:migrate
+# RAILS_ENV=production bin/rails db:seed
 
 exec "$@"
